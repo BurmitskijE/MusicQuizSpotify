a96da12a4274f48bf33ba7f3a398d22f85c306cb
BurmitskijE <124143025+BurmitskijE@users.noreply.github.com>
2025-01-16 19:20:13 +0000

    commit -m "Hintergrund und Anmeldebutton hinzugefügt"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './App.css'; // Stelle sicher, dass die CSS-Datei eingebunden ist
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const handleSpotifyLogin = () => {
+    // URL zur Spotify-Authentifizierung
+    window.location.href = 'https://accounts.spotify.com/authorize';
+  };
+
+  return (
+    <div className="app-container">
+      <button className="spotify-button" onClick={handleSpotifyLogin}>
+        Log in with Spotify
+      </button>
+    </div>
+  );
 }
 
 export default App;
